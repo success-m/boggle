19bc17e957b47d4081c4136fdbc50caa4e6e9e93
Success Malla <success.malla@gmail.com>
2019-12-01 14:13:48 +0545

    updated route to check if word is valid

--- api/app/controllers/words_controller.rb
+++ api/app/controllers/words_controller.rb
@@ -3,10 +3,23 @@ class WordsController < ApplicationController
 		headers['Access-Control-Allow-Origin'] = '*'
 
 		w = Word.find_by(word: params[:word])
-
-		result = {"result": "no data found"}
+		point = 0
+		result = {"result": "no data found", "point": point}
 		if w != nil
-			result = {"result": "success"}
+			
+			if w.word.length <= 4
+				point = 1
+			elsif w.word.length == 5
+				point = 2
+			elsif w.word.length == 6
+				point = 3
+			elsif w.word.length == 7
+				point = 4
+			elsif w.word.length >= 8
+				point = 11
+			end
+
+			result = {"result": "success", "point": point}
 		end
 
 
